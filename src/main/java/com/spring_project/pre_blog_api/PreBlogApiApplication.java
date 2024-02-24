b2530979d729133a9a09c626306fda32592ec643
Rifat-00 <rr.rifat22@gmail.com>
2024-02-24 14:14:16 +0600

    project initialization

---
+++ src/main/java/com/spring_project/pre_blog_api/PreBlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.spring_project.pre_blog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreBlogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreBlogApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring_project/pre_blog_api/PreBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_project.pre_blog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreBlogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
